be877cacc0053e8d6e31218ea08df99028a4b0c1
Cindyura36 <cindyaulia4462@gmail.com>
2024-11-03 15:59:40 +0700

    Update Soal2.pas

--- Soal2.pas
+++ Soal2.pas
@@ -1,1 +1,46 @@
-//ketik kode jawaban no 2 kamu disini
+program TotalBelanjaSayur; //nama program
+uses crt;
+var
+  jumlahPelanggan, pelangganKe, jumlahJenisSayur, jenisKe: Integer;
+  hargaSayur, jumlahSayur, totalBelanja: LongInt;
+
+begin
+  clrscr;
+  //input jumlah pelanggan
+  Write('Masukkan jumlah pelanggan: ');
+  ReadLn(jumlahPelanggan);
+  
+  pelangganKe := 1;
+
+  //mengulangi proses semua pelanggan
+  while pelangganKe <= jumlahPelanggan do
+  begin
+    WriteLn('Pelanggan ke-', pelangganKe, ':');
+    
+    //menerima input jumlah jenis sayur yang dibeli
+    Write('Masukkan jumlah jenis sayur yang dibeli: ');
+    ReadLn(jumlahJenisSayur);
+    totalBelanja := 0;
+
+    //hitung total belanja per-pelanggan
+    jenisKe := 1;
+    while jenisKe <= jumlahJenisSayur do
+    begin
+      Write('Masukkan harga sayur ke-', jenisKe, ': ');
+      ReadLn(hargaSayur);
+      Write('Masukkan jumlah sayur yang dibeli: ');
+      ReadLn(jumlahSayur);
+      
+      //hitung total untuk jenis sayur ini
+      totalBelanja := totalBelanja + (hargaSayur * jumlahSayur);
+      jenisKe := jenisKe + 1;
+    end;
+
+    //output total belanja untuk pelanggan ini
+    WriteLn('Total belanja untuk pelanggan ke-', pelangganKe, ': Rp ', totalBelanja);
+    
+    pelangganKe := pelangganKe + 1;
+  end;
+
+  ReadLn; //menunggu input untuk menutup program
+end.
