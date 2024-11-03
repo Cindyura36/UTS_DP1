1357edf388201a58a83a6a89c344cea4845a6de5
Cindyura36 <cindyaulia4462@gmail.com>
2024-11-03 16:00:24 +0700

    Update Soal3.pas

--- Soal3.pas
+++ Soal3.pas
@@ -1,1 +1,27 @@
-//ketik kode jawaban noo 3 kamu disini
+program Bintang ; //nama program
+uses crt ;
+
+var
+a,b,c : integer;
+
+begin
+    clrscr;
+    //input jumlah baris yang ingin diisi
+    write('masukan jumlah baris :'); readln(a);
+
+    //looping untuk baris
+    for b := 1 to a do
+    begin
+    //looping untuk kolom
+    for c := 1 to a do
+    begin
+        if(c = b) or (c =(a-b+1)) then //menentukan apa harus diisi bintang atau tidak
+        begin
+             write('*'); //menentukan bintang di posisi tertentu
+        end
+        else
+            write(' '); //memberikan spasi  
+        end;
+        writeln; //mengganti baris setelah terisi
+        end;
+end.
